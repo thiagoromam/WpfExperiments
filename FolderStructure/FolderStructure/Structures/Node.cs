dcc9ab37925ffa3f00044681c345fbd5e8166f2a
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2016-03-28 21:25:34 -0300

    expandable control added to folders

--- FolderStructure/FolderStructure/Structures/Node.cs
+++ FolderStructure/FolderStructure/Structures/Node.cs
@@ -1,14 +1,17 @@
-﻿using System.Windows;
+﻿using System.Collections.Specialized;
+using System.Windows;
 
 namespace FolderStructure.Structures
 {
     public class Node : DependencyObject
     {
         public static readonly DependencyProperty NameProperty;
+        public static readonly DependencyProperty IsExpandedProperty;
 
         static Node()
         {
             NameProperty = DependencyProperty.Register("Name", typeof(string), typeof(Node), new PropertyMetadata(default(string)));
+            IsExpandedProperty = DependencyProperty.Register("IsExpanded", typeof(bool), typeof(Node), new PropertyMetadata(true));
         }
         internal Node(object wrappedObject)
         {
@@ -18,6 +21,12 @@ namespace FolderStructure.Structures
         {
             IsFolder = true;
             Children = children;
+
+            Children.CollectionChanged += (s, e) =>
+            {
+                if (e.Action == NotifyCollectionChangedAction.Add)
+                    IsExpanded = true;
+            };
         }
 
         public string Name
@@ -26,6 +35,11 @@ namespace FolderStructure.Structures
             set { SetValue(NameProperty, value); }
         }
         public bool IsFolder { get; }
+        public bool IsExpanded
+        {
+            get { return (bool)GetValue(IsExpandedProperty); }
+            set { SetValue(IsExpandedProperty, value); }
+        }
         public NodeCollection Children { get; }
         public object WrappedObject { get; }
     }
